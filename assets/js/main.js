d1a842b5f2098b3c645d2a9784a7b22769a937e2
Bernard Jacques <Bernardjacques789@gmail.com>
2018-07-23 16:44:54 +0200

    Add Jobs

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let str = 0;
+let agi = 0;
+let int = 0;
+let dex = 0;
+let luk = 0;
+let sf = 0;
+
+
+
+
+document.getElementById("str").innerHTML = str;
+document.getElementById("agi").innerHTML = agi;
+document.getElementById("int").innerHTML = int;
+document.getElementById("dex").innerHTML = dex;
+document.getElementById("luk").innerHTML = luk;
+document.getElementById("sf").innerHTML = sf;
+
+
+
+
+
+
+let jobs = ["Bûcheron","Acrobate","Infirmier","Armurier","Artificier","Artilleur","Pilote d'Avion","Pilote de Course","Forgeron","Chasseur","Chimiste","Policier","Mécanicien","Cuisinier","Boxeur","Pompier"]
+
+let hobbies = ["Survivaliste", "Tir à l'Arc", "Tir en Stand", "Art-Martial", "  "]
+
+let Family = ["Célibataire", "Marié", "Veuf"]
